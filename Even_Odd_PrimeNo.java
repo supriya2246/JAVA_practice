bac925397881d3fe46e3bc11f7f6c2e9b7ca8385
supriya2246 <supriya4622@gmail.com>
2022-10-27 13:57:07 +0530

    file

---
+++ Even_Odd_PrimeNo.java
@@ -0,0 +1,15 @@
+public class Even_Odd_PrimeNo{
+	public static void main(String[]args){
+	
+		int num=17;
+		if( num%2==0)
+		{
+			System.out.println("Entern no is even");
+			System.out.println("Enter no is prime");
+			System.out.println("Enter no is naturalno");
+			}
+			else{
+				System.out.println("Entern no is odd");
+		}
+	}
+}
